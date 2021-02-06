b6d402438a22eb5f97fac22ebbfe33b675491ae5
Alexander Koskovich <zvnexus@outlook.com>
2021-02-06 00:55:01 -0700

    gms: Show Google's illustration on first page of finish setup.
     * Fills whitespace, looks nicer.
    
    Change-Id: I8624f1c5e4e3adffe33320babf676738963b33eb
    
    Former-commit-id: 0ed572a693a688ae79c07b502c405d802c7c1848

--- config.mk
+++ config.mk
@@ -24,6 +24,7 @@ PRODUCT_PACKAGES += \
     GmsContactsProviderOverlay \
     GmsSettingsOverlay \
     GmsSettingsProviderOverlay \
+    GmsSetupWizardOverlay \
     GmsSystemUIOverlay \
     GmsTelecommOverlay \
     GmsTelephonyOverlay \
